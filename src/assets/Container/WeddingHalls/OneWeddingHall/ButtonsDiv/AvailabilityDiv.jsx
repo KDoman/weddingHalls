217d94bd9de8675b29165b429da28e4adbd564cb
Kacper Domański <116498271+KDoman@users.noreply.github.com>
2023-08-07 19:50:33 +0200

    i want to do form for each button

--- src/assets/Container/WeddingHalls/OneWeddingHall/ButtonsDiv/AvailabilityDiv.jsx
+++ src/assets/Container/WeddingHalls/OneWeddingHall/ButtonsDiv/AvailabilityDiv.jsx
@@ -1,3 +1,31 @@
+import "./AvailabilityDiv.scss";
 export function AvailabilityDiv() {
-  return <h1>dostępność</h1>;
+  return (
+    <div className="availability-form ">
+      <h1 className=" text-center">Napisz do nas! </h1>
+      <form>
+        <input
+          id="name"
+          type="text"
+          placeholder="Imię"
+          className="input-form-style"
+        />
+        <input
+          id="email"
+          type="email"
+          placeholder="Email"
+          className="input-form-style"
+        />
+        <textarea
+          id="email"
+          placeholder="O co chcesz nas spytać?"
+          rows={10}
+          className="input-form-style"
+        />
+        <button type="button" className="button-form">
+          Wyślij wiadomość
+        </button>
+      </form>
+    </div>
+  );
 }
